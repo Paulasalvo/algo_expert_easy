b6d1620a419d092b262c38fe512f78238a760316
Paula Arroyo <p.arroyosalvo@gmail.com>
2024-01-28 00:36:28 -0300

    fun nonConstructibleChange, where currentChangeCreated+1 must be> than next coin

--- src/main/kotlin/4_sorted_squared_array.kt
+++ src/main/kotlin/4_sorted_squared_array.kt
@@ -1,3 +1,5 @@
+import kotlin.time.times
+
 /**
 Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new
 array of the same length with the squares of the original integers also sorted in ascending order.
@@ -8,9 +10,19 @@ Sample Output
  */
 
 fun sortedSquaredArray(array: List<Int>): List<Int> {
-   var doubleArray: List<Int> = array.map{
-           it*it
-   }
+    var left= 0
+    var right=array.size-1
+    val doubleArray = MutableList(array.size) { 0 }
+    for (i in doubleArray.size - 1 downTo 0)
+        if (array[left]*array[left]>array[right]*array[right]){
+            doubleArray[i]=array[left]*array[left]
+            left+=1
+        }else{
+            doubleArray[i]=array[right]*array[right]
+            right-=1
+        }
     return doubleArray
+   }
 
-}
+
+
