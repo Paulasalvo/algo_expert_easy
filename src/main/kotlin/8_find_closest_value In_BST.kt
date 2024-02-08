3eab36c60b68734d57047bef74e56570d0187def
Paula Arroyo <p.arroyosalvo@gmail.com>
2024-02-08 13:41:34 -0300

    exercise trees

---
+++ src/main/kotlin/8_find_closest_value In_BST.kt
@@ -0,0 +1,54 @@
+/**
+
+Write a function that takes in a Binary Search Tree (BST) and a target integer value and returns the closest
+value to that target value contained in the BST.You can assume that there will only be one closest value.
+Each BST  node has an integer value, a left  child node, and a right  child node. A node is said to be a valid BST  node if and only if it satisfies the BST
+property: its value  is strictly greater than the values of every node to its left; its value  is less than or equal to the values
+of every node to its right; and its children nodes are either valid BST  nodes themselves or None /null.
+Sample Input
+ tree  =   10
+         /     \
+        5      15
+      /   \   /   \
+     2     5 13   22
+   /           \
+  1            14
+target=12
+Sample Output= 13
+ */
+
+
+
+
+
+import kotlin.math.abs
+
+open class BST(value: Int) {
+    var value = value
+    var left: BST? = null
+    var right: BST? = null
+}
+
+fun findClosestValueInBst(tree: BST, target: Int): Int {
+    var closestValue = tree.value
+
+    var currentParent: BST? = tree
+
+    while (currentParent != null){
+        val diffCurrentValue = abs(currentParent.value - target)
+        val diffClosestValue = abs(closestValue - target)
+        if (diffCurrentValue < diffClosestValue){
+            closestValue = currentParent.value
+        }
+        if (target < currentParent.value && currentParent.left != null) {
+            currentParent = currentParent.left!!
+        } else if (target > currentParent.value && currentParent.right != null) {
+            currentParent = currentParent.right!!
+        }else{
+            currentParent = null
+        }
+
+    }
+
+    return closestValue
+}
